a2da473341b4ada3d8617a7e635a9b426e5b8d85
Jean McLeod <abricsltd@gmail.com>
2022-04-28 17:28:04 +0100

    added homeRoutes to Routes folder and tested. Working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const homeRoutes = require("./Routes/homeRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 80;
@@ -11,30 +11,9 @@ uuidv4();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(homeRoutes)
 
-// reads db.json and sets the data string to a variable
-let newNote;
-function readDb() {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    newNote = data;
-  });
-}
-readDb();
-
-//matching the fetch/get in server.js
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/db/db.json"))
-);
-
-app.post("/api/notes", (req, res) => {
+/*app.post("/api/notes", (req, res) => {
   let addedNote = JSON.parse(newNote); // changes array/string of characters into a JavasScript object JSON
   //addedNote is the array
   req.body.id = uuidv4();
@@ -53,7 +32,7 @@ app.delete("/api/notes/${id}", (req, res) => {
   console.log(req);
   //const {id} = req.params
   //res.send(id);
-});
+});*/
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
