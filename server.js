c6cb95e325acd47e075cf8855347e512129cd8d5
Jean McLeod <abricsltd@gmail.com>
2022-04-29 11:19:56 +0100

    cleaned up server code

--- server.js
+++ server.js
@@ -12,27 +12,7 @@ app.use(express.static("public"));
 app.use(api)
 app.use(homeRoutes)
 
-/*app.post("/api/notes", (req, res) => {
-  let addedNote = JSON.parse(newNote); // changes array/string of characters into a JavasScript object JSON
-  //addedNote is the array
-  req.body.id = uuidv4();
-  addedNote.push(req.body);
-
-  fs.writeFile("./db/db.json", JSON.stringify(addedNote), (err) =>
-    err ? console.error(err) : console.log(`New Information on Database`)
-  );
-
-  res.send(req.body);
-  readDb();
-});
-
-//delete note
-app.delete("/api/notes/${id}", (req, res) => {
-  console.log(req);
-  //const {id} = req.params
-  //res.send(id);
-});*/
-
+//fires the server into brower or PORT8080
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
 );
